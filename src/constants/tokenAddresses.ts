76e3d9e0b3c4e33e4114d04d788083c9d539957a
jamesbarnes97 <thomasthetrainrules-yyy@yahoo.com>
2020-11-20 00:37:46 +0000

    replace testnet addresses with mainnet addresses

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -6,11 +6,11 @@ export const yycrvUniLp = '0xb93Cc05334093c6B3b8Bfd29933bb8d5C031caBC'
 export const migrator = '0x72cfed9293cbfb2bfc7515c413048c697c6c811c'
 
 // harcode chainId for wallet provider lib
-export const chainId = 4
+export const chainId = 1
 
 
 // mainnet
-//export const strn = '0x90b426067be0b0ff5de257bc4dd6a4815ea03b5f'
+export const strn = '0x90b426067be0b0ff5de257bc4dd6a4815ea03b5f'
 export const strnEthLP = '0xa198f36e3648dd16e75d721f0c6516e00e9ca053'
 export const strnIncentivizer = '0x19Bf9bef453f57983319Eb3033a95a7aa7DB764d'
 // make sure to update the addresses in json files
@@ -20,9 +20,9 @@ export const strnXiotLP = '0xae14831467c8d0e3153aba2ea009bcdc485125a2'
 export const strnXiotIncentivizer = '0x0B864C8eD32457C83d3E35b47200085E96005425'
 
 // -- single staking
-//export const singleStrnPool = '0x69dc085C3C51F5ef74C737E817DD2A271e772B17'
+export const stxp = '0xdCF1d98F100445e1D74c3aedD0a90c565a8Da772'
 // src/yam-sdk/lib/clean_build/contracts/STXPIncentivizer.json
-//export const stxp = '0xdCF1d98F100445e1D74c3aedD0a90c565a8Da772'
+export const singleStrnPool = '0x69dc085C3C51F5ef74C737E817DD2A271e772B17'
 
 
 // rinkeby
@@ -36,9 +36,9 @@ export const strnXiotIncentivizer = '0x0B864C8eD32457C83d3E35b47200085E96005425'
 //export const strnXiotIncentivizer = '0xF70dE70202F326EA79f85C90e635F0abE338d73d'
 
 // single staking
-export const stxp = '0x3edd4De183b1DCa836FaA4e98D0A32c5a79eC9F6'
+//export const stxp = '0x3edd4De183b1DCa836FaA4e98D0A32c5a79eC9F6'
 // src/yam-sdk/lib/clean_build/contracts/STXPIncentivizer.json
-export const singleStrnPool = '0xCE8A1F067f636c4Fc2081E017aB6cE25182f5b26'
-export const strn = '0x9b0E1A31E8fA5E694CB607508B0cc546E0ACf8ea'
+//export const singleStrnPool = '0xCE8A1F067f636c4Fc2081E017aB6cE25182f5b26'
+//export const strn = '0x9b0E1A31E8fA5E694CB607508B0cc546E0ACf8ea'
 
 
