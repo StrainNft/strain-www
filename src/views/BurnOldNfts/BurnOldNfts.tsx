3e89484a864ef3cbcb7e0fc160c506289f4ec3eb
Svetlin Pavlov <svetlinpav@gmail.com>
2021-06-14 16:59:21 +0300

    Changing Notice on burnOldNft page based on wallet status

--- src/views/BurnOldNfts/BurnOldNfts.tsx
+++ src/views/BurnOldNfts/BurnOldNfts.tsx
@@ -1,40 +1,60 @@
-import React from 'react'
+import React, { useMemo } from "react";
 
-import {
-  Container,
-  Spacer,
-} from 'react-neu'
+import { Container, Spacer } from "react-neu";
 
-import styled from 'styled-components'
-import Page from 'components/Page'
-import StyledNotice from 'views/Common/StyledNotice'
-import StyledNft from 'views/Common/StyledNft'
-import useStrainNfts from 'hooks/useStrainNfts'
-import blankStrainNFT from '../../assets/shadyStrainNFT.png'
-import { DEFAULT_NFT_SIZE } from 'constants/poolValues'
+import styled from "styled-components";
+import Page from "components/Page";
+import StyledNotice from "views/Common/StyledNotice";
+import StyledNft from "views/Common/StyledNft";
+import useStrainNfts from "hooks/useStrainNfts";
+import blankStrainNFT from "../../assets/shadyStrainNFT.png";
+import { DEFAULT_NFT_SIZE } from "constants/poolValues";
+import { useWallet } from "use-wallet";
 
 const BurnOldNfts: React.FC = () => {
-
   const { oldStrainNftCollection } = useStrainNfts();
+
+  const { status } = useWallet();
+
+  const BurnOldNftsNotice = useMemo(() => {
+    if (status !== "connected") {
+      return (
+        <>
+          <StyledNotice messages={["Connect a Wallet to burn Old Nft's"]} />
+        </>
+      );
+    }
+    if (status === "connected") {
+      return (
+        <>
+          <StyledNotice messages={["You Don't Have Old Nft's"]} />
+        </>
+      );
+    }
+  }, [status]);
 
   return (
     <Page>
-        <BurnOldNftsContainer>
-          {oldStrainNftCollection.length <= 0 ?
-                  <>
-                  <img style={{paddingBottom: "30px"}} src={blankStrainNFT} alt="Blank Strain NFT" height={DEFAULT_NFT_SIZE} />
-                  <StyledNotice
-                    messages={["Connect a Wallet to burn Old Nft's"]}
-                  />
-                  </> : 
-            oldStrainNftCollection
-            .map((nft) => (
-                  <StyledNft key={nft.nftId} nft={nft} isBurnOldNftsPage={true} />
-            ))}
-        </BurnOldNftsContainer>
+      <BurnOldNftsContainer>
+        {oldStrainNftCollection.length <= 0 ? (
+          <>
+            <img
+              style={{ paddingBottom: "30px" }}
+              src={blankStrainNFT}
+              alt="Blank Strain NFT"
+              height={DEFAULT_NFT_SIZE}
+            />
+            {BurnOldNftsNotice}
+          </>
+        ) : (
+          oldStrainNftCollection.map((nft) => (
+            <StyledNft key={nft.nftId} nft={nft} isBurnOldNftsPage={true} />
+          ))
+        )}
+      </BurnOldNftsContainer>
     </Page>
-  )
-}
+  );
+};
 
 const BurnOldNftsContainer = styled.div`
   display: flex;
